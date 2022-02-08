9c0651a298a66b4e4f9effdea76bf6c675000163
AZR81 <zarkeshali81@gmail.com>
2022-02-08 01:34:25 +0000

    Added tests for 1B

--- test_geo.py
+++ test_geo.py
@@ -3,6 +3,7 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_within_radius
 from floodsystem.geo import rivers_by_station_number
+from floodsystem.geo import stations_by_distance
 from haversine import haversine as hav  # haversine function for distance
 
 def test_stations_within_radius():
@@ -63,9 +64,59 @@ def test_rivers_by_station_number():
     #Assert that first river not in original output less has less stations than the last river in the first output list
     assert next_output[len(output)][1] < output[len(output) - 1][1]
 
-    
 
- 
+def test_stations_by_distance():
+    # Build list of stations
+    stations = build_station_list()
 
+    # Build list of stations and their corresponding distances
+    sorted_stations = stations_by_distance(stations, (52.2053, 0.1218))
 
+    # Assert that the stations are in order of increasing distance
+    for index in range(len(sorted_stations) - 1):
+        assert sorted_stations[index][1] <= sorted_stations[index + 1][1]
 
+    # List of the 10 closest stations to Cambridge city centre
+    known_closest_stations = [('Cambridge Jesus Lock', 'Cambridge', 0.8402364350834995),
+                              ('Bin Brook', 'Cambridge', 2.502274086951454),
+                              ("Cambridge Byron's Pool", 'Grantchester', 4.0720438555077125),
+                              ('Cambridge Baits Bite', 'Milton', 5.115589516578674),
+                              ('Girton', 'Girton', 5.227070345811418),
+                              ('Haslingfield Burnt Mill', 'Haslingfield', 7.044388165868453),
+                              ('Oakington', 'Oakington', 7.128249171700346),
+                              ('Stapleford', 'Stapleford', 7.265694306995238),
+                              ('Comberton', 'Comberton', 7.7350743760373675),
+                              ('Dernford', 'Great Shelford', 7.993861351711722)]
+
+    # Store the 10 closest stations in the same format at the known ones
+    calculated_closest_stations = [(station[0].name, station[0].town, station[1]) for station in sorted_stations[:10]]
+
+    # Assert that the calculated closest stations are the same as the known ones
+    for known, calculated in zip(known_closest_stations, calculated_closest_stations):
+        assert known[0] == calculated[0]
+        assert known[1] == calculated[1]
+        # Assert the difference in distance is less than or equal to one meter
+        assert abs(known[2] - calculated[2]) * 1000 <= 1
+
+    # List of the 10 furthest stations from Cambridge city centre
+    known_furthest_stations = [('Boscadjack', 'Wendron', 440.0026482838576),
+                               ('Gwithian', 'Gwithian', 442.05491558132354),
+                               ('Helston County Bridge', 'Helston', 443.37824966454974),
+                               ('Loe Pool', 'Helston', 445.07184458260684),
+                               ('Relubbus', 'Relubbus', 448.64944322554413),
+                               ('St Erth', 'St Erth', 449.03415711886015),
+                               ('St Ives Consols Farm', 'St Ives', 450.0734690482922),
+                               ('Penzance Tesco', 'Penzance', 456.3857579793324),
+                               ('Penzance Alverton', 'Penzance', 458.5766422710278),
+                               ('Penberth', 'Penberth', 467.53367291629183)]
+
+    # Store the 10 furthest stations in the same format at the known ones
+    calculated_furthest_stations = [(station[0].name, station[0].town, station[1]) for station in
+                                    sorted_stations[-10:]]
+
+    # Assert that the calculated closest stations are the same as the known ones
+    for known, calculated in zip(known_furthest_stations, calculated_furthest_stations):
+        assert known[0] == calculated[0]
+        assert known[1] == calculated[1]
+        # Assert the difference in distance is less than or equal to one meter
+        assert abs(known[2] - calculated[2]) * 1000 <= 1
